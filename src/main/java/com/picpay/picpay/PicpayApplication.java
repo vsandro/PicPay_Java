270b47cd87004262f0797cb362bce62c5c3909b3
Sandro Vieira <vsandro@gmail.com>
2024-03-27 19:05:23 +0000

    feat: first commit

---
+++ src/main/java/com/picpay/picpay/PicpayApplication.java
@@ -0,0 +1,15 @@
+package com.picpay.picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PicpayApplication.class, args);
+		System.out.println("PicPay Application");
+	}
+
+}
