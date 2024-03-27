77d83c41e09d6caa6937f60485215b7c91010331
Sandro Vieira <vsandro@gmail.com>
2024-03-27 19:05:52 +0000

    feat: create user

---
+++ src/main/java/com/picpay/picpay/domain/user/User.java
@@ -0,0 +1,33 @@
+package com.picpay.picpay.domain.user;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity(name="users")
+@Table(name="users")
+@Data()
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firsName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+}
---
+++ src/main/java/com/picpay/picpay/domain/user/UserType.java
@@ -0,0 +1,6 @@
+package com.picpay.picpay.domain.user;
+
+public enum UserType {
+    COMMON,
+    MERCHANT
+}
