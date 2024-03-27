3243ec8ea3e2ea5563df3279facde8a39661bbc1
Sandro Vieira <vsandro@gmail.com>
2024-03-27 20:04:22 +0000

    feat: create user service

--- src/main/java/com/picpay/picpay/services/UserService.java
+++ src/main/java/com/picpay/picpay/services/UserService.java
@@ -4,6 +4,7 @@ import com.picpay.picpay.domain.user.User;
 import com.picpay.picpay.domain.user.UserType;
 import com.picpay.picpay.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.ExpressionException;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -22,6 +23,13 @@ public class UserService {
         if (sender.getBalance().compareTo(amount) < 0) {
             throw new Exception("Saldo insuficiente!");
         }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(() -> new ExpressionException("Usuário não encontrado!"));
+    }
+    public void saveUser(User user) {
+        this.repository.save(user);
     }
 
 }
